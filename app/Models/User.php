0218e251ab457c74f8ce49a8b95211eedac42fbd
Leonardo Augustus <leoaugusto45@gmail.com>
2025-10-20 20:34:50 -0300

    laradumps and logs

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements Auditable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
+    use \OwenIt\Auditing\Auditable;
     use HasFactory;
     use Notifiable;
 
